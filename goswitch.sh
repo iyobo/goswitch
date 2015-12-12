cb3ee8523847bc7046a97a175461cfc8a4b3009f
Iyobo Eki <phil.eki@gmail.com>
2015-12-11 20:21:23 -0500

    v0.0.4
    Now edits bash_profile instead of bashrc

--- goswitch.sh
+++ goswitch.sh
@@ -4,7 +4,7 @@
 # A little script to help you manage your various Golang Projects and their individual GoPaths.
 # Simply put this script in the root directory of all your various GoPaths and run it with "source goswitch.sh" to set that directory as your current active go project directory (or gopath).
 # The script will also add the bin folder of your project into your system path variable so you can run all your bins as you would any command line command e.g. 'beego run'
-echo "  GoSwitch v0.0.3"
+echo "  GoSwitch v0.0.4"
 echo "    The GOPATH switcher by Iyobo Eki"
 path_append ()  { path_remove $1; export PATH="$PATH:$1"; }
 path_prepend () { path_remove $1; export PATH="$1:$PATH"; }
@@ -13,16 +13,16 @@ path_remove ()  { export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'
 #vars
 newgopath=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 oldgopath=$GOPATH
-gsbashrc=~/.bashrc
-#gsbashprofile=~/.bash_profile
+#gsbashrc=~/.bashrc
+gsbashprofile=~/.bash_profile
 
 path_remove $oldgopath/bin
 
 #Remove from bash inits
-grep -v "source $GOPATH/goswitch.sh" $gsbashrc > bashtemp
-mv bashtemp $gsbashrc
-#grep -v "source $GOPATH/goswitch.sh" $gsbashprofile > bashptemp
-#mv bashptemp $gsbashprofile
+#grep -v "source $GOPATH/goswitch.sh" $gsbashrc > bashtemp
+#mv bashtemp $gsbashrc
+grep -v "source $GOPATH/goswitch.sh" $gsbashprofile > bashptemp
+mv bashptemp $gsbashprofile
 
 # touch gopath.old
 
@@ -35,8 +35,8 @@ export GOPATH=$newgopath
 path_append $GOPATH/bin
 export GOROOT=/usr/local/go
 
-echo "source $GOPATH/goswitch.sh" >> $gsbashrc
-#echo "source $GOPATH/goswitch.sh" >> $gsbashprofile
+#echo "source $GOPATH/goswitch.sh" >> $gsbashrc
+echo "source $GOPATH/goswitch.sh" >> $gsbashprofile
 
 
 echo "    Your Gopath is now $(tput setaf 6)$GOPATH.$(tput sgr 0) "

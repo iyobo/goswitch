cfab97928fa0c5a91cc9171f558292aadded28aa
Iyobo Eki <phil.eki@gmail.com>
2015-12-11 20:32:07 -0500

    Removed obnoxious echos.

--- goswitch.sh
+++ goswitch.sh
@@ -4,8 +4,8 @@
 # A little script to help you manage your various Golang Projects and their individual GoPaths.
 # Simply put this script in the root directory of all your various GoPaths and run it with "source goswitch.sh" to set that directory as your current active go project directory (or gopath).
 # The script will also add the bin folder of your project into your system path variable so you can run all your bins as you would any command line command e.g. 'beego run'
-echo "  GoSwitch v0.0.4"
-echo "    The GOPATH switcher by Iyobo Eki"
+#echo "  GoSwitch v0.0.4"
+#echo "    The GOPATH switcher by Iyobo Eki"
 path_append ()  { path_remove $1; export PATH="$PATH:$1"; }
 path_prepend () { path_remove $1; export PATH="$1:$PATH"; }
 path_remove ()  { export PATH=`echo -n $PATH | awk -v RS=: -v ORS=: '$0 != "'$1'"' | sed 's/:$//'`; }
@@ -39,9 +39,9 @@ export GOROOT=/usr/local/go
 echo "source $GOPATH/goswitch.sh" >> $gsbashprofile
 
 
-echo "    Your Gopath is now $(tput setaf 6)$GOPATH.$(tput sgr 0) "
-echo  "    Your project's bin folder ($(tput setaf 6)$GOPATH/bin$(tput sgr 0)) has been $(tput setaf 3)added to your PATH$(tput sgr 0)."
-echo  "    You can now easily run execs in your project's bin folder e.g. $(tput setaf 11)beego run$(tput sgr 0)"
-echo "    Who says you don't need to set GOROOT? Your new GOROOT is $(tput setaf 6)$GOROOT.$(tput sgr 0) "
-
+#echo "    Your Gopath is now $(tput setaf 6)$GOPATH.$(tput sgr 0) "
+#echo  "    Your project's bin folder ($(tput setaf 6)$GOPATH/bin$(tput sgr 0)) has been $(tput setaf 3)added to your PATH$(tput sgr 0)."
+#echo  "    You can now easily run execs in your project's bin folder e.g. $(tput setaf 11)beego run$(tput sgr 0)"
+#echo "    Who says you don't need to set GOROOT? Your new GOROOT is $(tput setaf 6)$GOROOT.$(tput sgr 0) "
+echo "  GoSwitch: Active Go Project ($(tput setaf 6)$GOPATH$(tput sgr 0)) "
 #Source from bashrc
